ad7a0371ad27be8bf303aeefa62496f79041b931
emilvincic <evinci00@fesb.hr>
2022-03-16 19:28:57 +0100

    Add new method

---
+++ NewClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_EmilVincic
+{
+    class NewClass
+    {
+        int Main()
+        {
+            return 4;
+        }
+    }
+}
